26501ce9241ad5e52b127fd688390b73c5544ee4
Cesasie <Cesasie@DESKTOP-4F3U2I3>
2023-04-21 15:18:47 +0300

    Proje dosyası ekle.

---
+++ Fibonacci/Fibonacci.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+
+	int a = 1;
+	int b = 1;
+	int n;
+	cout << "Sayi degeri giriniz:";
+	cin >> n;
+	if (n == 0);
+	else if (n == 1)
+		cout << 1 << endl;
+
+	else {
+		cout << a << endl << b << endl;
+		for (int i = 0; i < n - 2; i++) {
+			int c = a + b;
+			a = b;
+			b = c;
+			cout << c << endl;
+
+		}
+
+	}
+	return 0;
+}
